3480a311b978d2caad2e165e52787501de0bc0b7
Jaime Moscoso <jaime.moscoso@gmail.com>
2023-01-31 16:01:22 -0300

    Add new version with encrypter field support

---
+++ SupervisorCredentialManager/Proyecto/V2/SupervisorProfileManager/ConsoleApp1/cGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SupervisorProfileManager
+{
+    public class cGroup
+    {
+        public string ID { get; set; }
+        public string Denied { get; set; }
+        public string Name { get; set; }
+        public List<cUser> Users { get; set; }
+    }
+}
